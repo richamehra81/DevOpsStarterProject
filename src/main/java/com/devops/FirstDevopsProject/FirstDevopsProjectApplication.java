359441d3b782e5ddc623c71b7790c2dfadbbad30
richa <richamehra81@gmail.com>
2021-01-19 13:46:23 +0530

    First Commit

---
+++ src/main/java/com/devops/FirstDevopsProject/FirstDevopsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.devops.FirstDevopsProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstDevopsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstDevopsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devops/FirstDevopsProject/FirstDevopsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.FirstDevopsProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstDevopsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
